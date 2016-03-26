db5ec6068ada6baff2bd185a60eeb4bc5cd14494
zhangsikai123 <Zhangsikai123@tamu.edu>
2016-03-26 13:03:45 -0500

    db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326075022) do
+ActiveRecord::Schema.define(version: 20160326175443) do
 
   create_table "catagories", force: :cascade do |t|
     t.integer  "trip_good_id"

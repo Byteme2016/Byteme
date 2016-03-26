87ba42021c67a4dede811fb27eb6c17f8919b006
Lihao Zou <loe.zou@gmail.com>
2016-03-26 08:55:23 -0500

    searchV2

--- app/controllers/trip_goods_controller.rb
+++ app/controllers/trip_goods_controller.rb
@@ -7,15 +7,10 @@ class TripGoodsController < ApplicationController
 	  if params[:trip_good] == nil
 	  	render 'show'
 	  else	
-		city_name = params[:trip_good][:city]
-		city = City.find_by_name(city_name)
-
-		if city
-			city_id = city.id
-		else
-			return
-		end
-
+	  	@city = params[:trip_good][:city]
+	  	@price = params[:trip_good][:price]
+	  	
+	  	# Build date strings for query
 		start_year = params[:trip_good]["beginning_date(1i)"]
 		start_month = params[:trip_good]["beginning_date(2i)"].rjust(2,'0')
 		start_day = params[:trip_good]["beginning_date(3i)"].rjust(2,'0')
@@ -27,17 +22,46 @@ class TripGoodsController < ApplicationController
 		start_date = start_year,start_month,start_day
 		end_date = end_year,end_month,end_day
 
-		logger.debug "city_id is #{city_id}"
-		logger.debug "the start day is #{start_date.join}"
-		logger.debug "the end day is #{end_date.join}"
+		if start_year != "" and start_month != "00" and start_day != "00" 
+			@start_date = start_date.join('-').to_date
+		end
 
-		if start_year=="" || start_month=="" || start_day=="" ||
-			end_year=="" || end_month=="" || end_day==""
+		if end_year != "" and end_month != "00" and end_day != "00"
+			@end_date = end_date.join('-').to_date
+		end
+
+		city_name = params[:trip_good][:city]
+		city = City.find_by_name(city_name)
+
+		# Find if the city exits or not, if not return directly
+		if city
+			city_id = city.id
+		else
 			return
 		end
 
-		@trip_goods = TripGood.where("city_id = ? and beginning_date <= ? and end_date >= ?",city_id,start_date.join,end_date.join)
+		price = params[:trip_good][:price]
 
+		# Find if the price is a valid number or not, if not set to null string
+		if not price =~ /[^a-zA-Z ]+/
+			price = ""
+		end
+		
+		#logger.debug "city_id is #{city_id}"
+		#logger.debug "the start day is #{start_date.join}"
+		#logger.debug "the end day is #{end_date.join}"
+
+		if !@start_date || !@end_date || @start_date>@end_date 
+			return
+		end
+
+		if price == ""
+			@trip_goods = TripGood.where("city_id = ? and beginning_date <= ? and end_date >= ? and on_shelf = 1",
+			city_id,start_date.join('-').to_date,end_date.join('-').to_date)
+		else			
+			@trip_goods = TripGood.where("city_id = ? and beginning_date <= ? and end_date >= ? and on_shelf = 1 and price <= ?",
+			city_id,start_date.join('-').to_date,end_date.join('-').to_date,price.to_i())
+		end
 	  end
 	end
 

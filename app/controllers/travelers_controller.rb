e537ef849c3f18f526adc80c4345314a87d0d0ce
zhangsikai123 <Zhangsikai123@tamu.edu>
2016-03-26 13:23:31 -0500

    commit

--- app/controllers/travelers_controller.rb
+++ app/controllers/travelers_controller.rb
@@ -8,9 +8,18 @@ class TravelersController < ApplicationController
   end
   
   def edit
-      @traveler = Traveler.find params[:id]
+      @traveler = Traveler.find_by(id: session[:id])  
   end
-  
+  def update
+    @traveler = Traveler.find_by(id: session[:id])
+    if @traveler.update(traveler_params)
+      flash[:danger]="Update succeeded!"      
+      redirect_to @traveler
+    else 
+      flash[:danger]="Update failed!" 
+      redirect_to @traveler     
+    end 
+  end
   def create
     @traveler = Traveler.new(traveler_params)
     if @traveler.save
@@ -23,7 +32,7 @@ class TravelersController < ApplicationController
   
   def show
     @traveler = Traveler.find_by(id: session[:id])
-    flash.now[:notice] = 'Welcome back! Dear '+@traveler.first_name
+    flash.now[:notice] = 'Welcome back! Dear '+ @traveler.first_name
   end
   def traveler_params
    params.require(:traveler).permit(:first_name,:middle_name,:last_name,:password,:password_confirmation,:email,:username,:gender,:age,:photo,:contact_number,:alter_number,:street_address,:city,:state,:zip_code,:country)
--- app/models/traveler.rb
+++ app/models/traveler.rb
@@ -2,4 +2,12 @@ class Traveler < ActiveRecord::Base
   has_many :ordered_trips
   has_many :paying_methods
   has_secure_password
+  validates :email,:first_name,:last_name,:contact_number,
+  presence: true,
+  :on => [ :create ]
+  validates :password,
+  length: { in: 6..20,
+             to_short: "your password should be at least %{count} characters",
+             to_long: "your password should be at most %{count} characters" },
+   :on => [ :create ]          
 end

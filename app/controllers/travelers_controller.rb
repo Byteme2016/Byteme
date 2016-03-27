5c48ee9b79913080e7d30bfb79813714915d66fe
zhangsikai123 <Zhangsikai123@tamu.edu>
2016-03-27 07:24:11 -0500

    front_end

--- app/controllers/travelers_controller.rb
+++ app/controllers/travelers_controller.rb
@@ -32,7 +32,9 @@ class TravelersController < ApplicationController
   
   def show
     @traveler = Traveler.find_by(id: session[:id])
-    flash.now[:notice] = 'Welcome back! Dear '+ @traveler.first_name
+    if !flash[:danger]
+     flash.now[:notice] = 'Welcome back! Dear '+ @traveler.first_name
+    end
   end
   def traveler_params
    params.require(:traveler).permit(:attachment,:first_name,:middle_name,:last_name,:password,:password_confirmation,:email,:username,:gender,:age,:photo,:contact_number,:alter_number,:street_address,:city,:state,:zip_code,:country)
